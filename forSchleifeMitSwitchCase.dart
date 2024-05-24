f9720bafce27ac80cd50af8f59c42415154730e5
FerushT <ferush82@gmail.com>
2024-05-24 15:56:26 +0200

    Aufgabe 6

---
+++ forSchleifeMitSwitchCase.dart
@@ -0,0 +1,22 @@
+void main() {
+  for (int i = 1; i <= 7; i++) {
+    switch (i) {
+      case 1:
+        print("Montag");
+      case 2:
+        print("Dienstag");
+      case 3:
+        print("Mittwoch");
+      case 4:
+        print("Donnerstag");
+      case 5:
+        print("Freitag");
+      case 6:
+        print("Samstag");
+      case 7:
+        print("Sonntag");
+      default:
+        print("ungültige Eingabe");
+    }
+  } 
+}
